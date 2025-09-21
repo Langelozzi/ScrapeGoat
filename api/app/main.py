19ba7aa40ca9fd1de647c446746bdd3397e4d2c4
kate <katezraly@gmail.com>
2025-09-21 14:47:57 -0700

    Retrieve mock dom tree from backend

--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.modules.scraper.routes import router as scraper_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["POST"],
+    allow_headers=["Accept","Content-Type"],
+)
 
 OPEN_API_PREFIX = "/api/v1"
 # Register the routes
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,49 @@ import { useState } from 'react'
 
 function App() {
   const [url, setUrl] = useState("");
+  const [tree, setTree] = useState(null);
 
+  const buildTree = async function() {
+    fetch('http://127.0.0.1:8000/api/v1/scraper/dom-tree/build', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ url })
+    })
+      .then(response => response.json())
+      .then(json => { setTree(json.root); console.log(json); })
+      .catch(error => console.error(error));
+  }
+  
   return (
     <div className="min-h-screen flex flex-col items-center justify-start bg-gradient-to-br from-black via-purple-900 to-black text-white p-8">
       {/* Title */}
       <h1 className="text-6xl text-emerald-200 font-extrabold mt-20 mb-10">Scrapegoat</h1>
 
-      {/* Input Bar */}
-      <input
-        type="text"
-        placeholder="Enter website URL..."
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-        className="w-200 p-4 text-lg bg-white rounded-2xl text-black shadow-lg focus:outline-none focus:ring-4 focus:ring-purple-400"
-      />
+      {/* Input */}
+      <div className="flex space-x-4">
+        <input
+          type="text"
+          placeholder="Enter website URL..."
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          className="w-96 p-4 text-lg bg-white rounded-2xl text-black shadow-lg focus:outline-none focus:ring-4 focus:ring-purple-400"
+        />
+
+        <button
+          className="px-8 py-4 text-lg font-bold bg-white rounded-2xl text-black shadow-lg focus:outline-none focus:ring-4 focus:ring-purple-400 hover:bg-purple-200 transition"
+          onClick={buildTree}
+        >
+          Submit
+        </button>
+      </div>
+
+      {/* Output */}
+      <div>
+        {tree ? JSON.stringify(tree) : 'no tree :('}
+      </div>
     </div>
   )
 }
