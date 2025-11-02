8d44cb8bc43fa89d276b3b64f42c5a231c6faf0c
Langelozzi <lucasangelozzi32@gmail.com>
2025-11-02 10:13:30 -0800

    Add endpoint for exporting to json

--- api/app/modules/scraper/routes.py
+++ api/app/modules/scraper/routes.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Body, Depends, Query
+from fastapi.responses import StreamingResponse
 
 from app.modules.scraper.models import BuildDomTreeRequest
+from app.shared.helpers.dict_helpers import to_json_stream
 from app.shared.models.html import DOMTree
 from app.shared.models.scrape import ScrapeConfig, ScrapedDataset
 from .service import build_dom_tree, scrape
@@ -17,13 +19,33 @@ def get_health_check():
 
 @router.post("/dom-tree/build")
 def post_build_dom_tree(
-    req: BuildDomTreeRequest, current_user: AuthUser = Depends(require_auth)
+    req: BuildDomTreeRequest, _: AuthUser = Depends(require_auth)
 ) -> DOMTree:
     return build_dom_tree(req.url)
 
 
 @router.post("/scrape")
 def post_scrape(
-    config: ScrapeConfig, current_user: AuthUser = Depends(require_auth)
+    config: ScrapeConfig, _: AuthUser = Depends(require_auth)
 ) -> ScrapedDataset:
     return scrape(config)
+
+
+@router.post("/export/json")
+def export_json(
+    filename: str = Query(
+        "data.json", description="Filename for the downloaded JSON file."
+    ),
+    data: dict = Body(..., description="JSON data to include in the file"),
+    _: AuthUser = Depends(require_auth),
+):
+    if not filename.lower().endswith(".json"):
+        filename += ".json"
+
+    file_stream = to_json_stream(data)
+
+    return StreamingResponse(
+        file_stream,
+        media_type="application/octet-stream",
+        headers={"Content-Disposition": f'attachment; filename="{filename}"'},
+    )
